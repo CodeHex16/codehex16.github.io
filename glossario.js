ceb4d04b9a3f77d2c10b02591693532fb0a0d02b
bazz333 <matteobazzan333@gmail.com>
2024-12-02 10:59:42 +0100

    rimossa funzione inutile; sistemato il padding dello scroll glossario

--- glossario.js
+++ glossario.js
@@ -1,29 +1,28 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    await fetchGlossario();
+document.addEventListener("DOMContentLoaded", async () => {
+	await fetchGlossario();
 });
 
 async function fetchGlossario() {
-    try {
-        const response = await fetch('/docs/glossario/glossario.txt');
-        const text = await response.text();
-        const lines = text.split('\n');
-        let letteraCorrente = '';
+	try {
+		const response = await fetch("/docs/glossario/glossario.txt");
+		const text = await response.text();
+		const lines = text.split("\n");
+		let letteraCorrente = "";
 
-        lines.forEach(line => {
-            if (line.startsWith('= ')) {
-                letteraCorrente = line.replace('= ', '').trim().toLowerCase();
-            } else if (line.startsWith('== ')) {
-                const termine = line.replace('== ', '').trim();
-                const definizione = lines[lines.indexOf(line) + 1].trim(); // da capire quando si arriva alla fine della definizione
-                const container = document.getElementById(letteraCorrente);
-                if (container) {
-                    container.innerHTML += `<dt id="${termine}">${termine}</dt><dd>${definizione}</dd>`;
-                    container.parentElement.classList.remove('hidden');
-                }
-            }
-        });
-        showNotEmptyLetters();
-    } catch (error) {
-        console.error('Errore nel caricamento del glossario:', error);
-    }
+		lines.forEach((line) => {
+			if (line.startsWith("= ")) {
+				letteraCorrente = line.replace("= ", "").trim().toLowerCase();
+			} else if (line.startsWith("== ")) {
+				const termine = line.replace("== ", "").trim();
+				const definizione = lines[lines.indexOf(line) + 1].trim(); // da capire quando si arriva alla fine della definizione
+				const container = document.getElementById(letteraCorrente);
+				if (container) {
+					container.innerHTML += `<dt id="${termine}">${termine}</dt><dd>${definizione}</dd>`;
+					container.parentElement.classList.remove("hidden");
+				}
+			}
+		});
+	} catch (error) {
+		console.error("Errore nel caricamento del glossario:", error);
+	}
 }
--- script.js
+++ script.js
@@ -1,72 +1,85 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    await fetchDocuments();
+document.addEventListener("DOMContentLoaded", async () => {
+	await fetchDocuments();
 });
 
 async function fetchDocuments() {
-    const repoOwner = 'codehex16';
-    const repoName = 'documentazione';
-    const url = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/`;
-    const docsUrl = '/docs/'
+	const repoOwner = "codehex16";
+	const repoName = "documentazione";
+	const url = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/`;
+	const docsUrl = "/docs/";
 
-    try {
-        const cachedData = sessionStorage.getItem('pdfFiles');
-        let pdfFiles = cachedData ? JSON.parse(cachedData) : [];
-        if (!cachedData) {
-            const promiseInterni = fetch(url + "verbali/interni");
-            const promiseEsterni = fetch(url + "verbali/esterni");
-            const promiseCandidatura = fetch(url + "1 - candidatura");
-            const promiseRTB = fetch(url + "2 - RTB");
+	try {
+		const cachedData = sessionStorage.getItem("pdfFiles");
+		let pdfFiles = cachedData ? JSON.parse(cachedData) : [];
+		if (!cachedData) {
+			const promiseInterni = fetch(url + "verbali/interni");
+			const promiseEsterni = fetch(url + "verbali/esterni");
+			const promiseCandidatura = fetch(url + "1 - candidatura");
+			const promiseRTB = fetch(url + "2 - RTB");
 
-            const [interniResponse, esterniResponse, candidaturaResponse, rtbResponse] = await Promise.all([promiseInterni, promiseEsterni, promiseCandidatura, promiseRTB]);
-            let data = await interniResponse.json();
-            data = data.concat(await esterniResponse.json());
-            data = data.concat(await candidaturaResponse.json());
-            data = data.concat(await rtbResponse.json());
-            
-            console.log(data);
+			const [
+				interniResponse,
+				esterniResponse,
+				candidaturaResponse,
+				rtbResponse,
+			] = await Promise.all([
+				promiseInterni,
+				promiseEsterni,
+				promiseCandidatura,
+				promiseRTB,
+			]);
+			let data = await interniResponse.json();
+			data = data.concat(await esterniResponse.json());
+			data = data.concat(await candidaturaResponse.json());
+			data = data.concat(await rtbResponse.json());
 
-            for (const item of data) {
-                if (item.type === 'file' && item.name.endsWith('.pdf')) {
-                    pdfFiles.push(item);
-                }
-            }
+			console.log(data);
 
-            sessionStorage.setItem('pdfFiles', JSON.stringify(pdfFiles));
-        }
+			for (const item of data) {
+				if (item.type === "file" && item.name.endsWith(".pdf")) {
+					pdfFiles.push(item);
+				}
+			}
 
-        pdfFiles.forEach(pdf => {
-            let ulElement;
-            //console.log(pdf.path);
-            if (pdf.path.includes("verbali/interni")) {
-                ulElement = document.querySelector("#interni .link-file");
-            } else if (pdf.path.includes("verbali/esterni")) {
-                ulElement = document.querySelector("#esterni .link-file");
-            } else if (pdf.path.includes("candidatura")) {
-                ulElement = document.querySelector("#candidatura .link-file");
-            }else if (pdf.path.includes("RTB")) {
-                ulElement = document.querySelector("#rtb .link-file");
-            }
+			sessionStorage.setItem("pdfFiles", JSON.stringify(pdfFiles));
+		}
 
-            if (ulElement) {
-                let nameDate = parseName(pdf.name);
-                ulElement.innerHTML += `<li><a href="${docsUrl+pdf.path}" target="_blank" class="file-link">${nameDate[0]}</a>${nameDate[1] ? `<span class="file-date">${nameDate[1]}</span>` : ''}</li>`;
-            }
-        });
-    } catch {
-        console.error('Unable to fetch documents');
-    }
+		pdfFiles.forEach((pdf) => {
+			let ulElement;
+			//console.log(pdf.path);
+			if (pdf.path.includes("verbali/interni")) {
+				ulElement = document.querySelector("#interni .link-file");
+			} else if (pdf.path.includes("verbali/esterni")) {
+				ulElement = document.querySelector("#esterni .link-file");
+			} else if (pdf.path.includes("candidatura")) {
+				ulElement = document.querySelector("#candidatura .link-file");
+			} else if (pdf.path.includes("RTB")) {
+				ulElement = document.querySelector("#rtb .link-file");
+			}
+
+			if (ulElement) {
+				let nameDate = parseName(pdf.name);
+				ulElement.innerHTML += `<li><a href="${docsUrl + pdf.path}" target="_blank" class="file-link">${nameDate[0]}</a>${nameDate[1] ? `<span class="file-date">${nameDate[1]}</span>` : ""}</li>`;
+			}
+		});
+	} catch {
+		console.error("Unable to fetch documents");
+	}
 }
 
 function parseName(name) {
-    let fileName;
-    if (name.includes("_")) {
-        let docDate = name.split('_')[0];
-        let docName = name.split('_')[1].replace('.pdf', '').replaceAll("-", " ");
-        fileName = [docName, docDate];
-    } else {
-        let docName = name.replace('.pdf', '').replaceAll("-", " ");
-        fileName = [docName];
-    }
+	let fileName;
+	if (name.includes("_")) {
+		let docDate = name.split("_")[0];
+		let docName = name
+			.split("_")[1]
+			.replace(".pdf", "")
+			.replaceAll("-", " ");
+		fileName = [docName, docDate];
+	} else {
+		let docName = name.replace(".pdf", "").replaceAll("-", " ");
+		fileName = [docName];
+	}
 
-    return fileName;
+	return fileName;
 }
